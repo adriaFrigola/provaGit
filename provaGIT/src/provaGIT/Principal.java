96dba66dd21bcf1a85120cf127694fb36ba86eea
adrfrg <adrfrg@adrfrg-Legion>
2024-10-03 15:12:38 +0200

    comit and push

--- provaGIT/src/provaGIT/Principal.java
+++ provaGIT/src/provaGIT/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("PRIMER COMMIT");
+		System.out.println("SEGON COMMIT");
 	}
 
 }
